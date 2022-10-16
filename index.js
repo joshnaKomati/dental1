e3d4af7466a4aaf77c2d6853483da6348309799f
Sravan komati <sravankomati12@gmail.com>
2022-10-16 21:19:53 +0530

    second msg

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
-const express=require("express")
-const app=express()
-app.use(express.json())
-app.use(express.urlencoded({extended:true}))
-require("./configure/db")
-const mainRouter=require("./router/mainRouter")
-app.use("/api",mainRouter)
+const express = require('express');
+const bcrypt = require('bcrypt');
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+require('./configure/db');
+const mainRouter = require('./router/mainRouter');
+app.use('/api', mainRouter);
 
-app.listen(4000,()=>{
-    console.log("server is connected");
-})
+app.listen(4000, () => {
+  console.log('server is connected');
+});
